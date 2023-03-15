ede17ad5d037fa1e8eeca453205ae86d82e6e033
AvatarAxel <zs20015691@estudiantes.uv.mx>
2023-03-14 18:25:12 -0600

    Add class

---
+++ src/util/Random.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.security.SecureRandom;
+
+/**
+ *
+ * @author michikato
+ */
+public class Random {
+    
+    public String passwordGenerator(){
+        String password = null;
+        SecureRandom random = new SecureRandom();
+        
+        return password;
+        
+    }
+    
+    public String verificationCodeGenerator() {
+        String verificationCode = null;
+        SecureRandom random = new SecureRandom();
+        
+        
+        return verificationCode;
+    }
+}
