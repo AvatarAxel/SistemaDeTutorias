4b7b72aa165f92e1b13cd3fdf19a8614fceedaa6
ValeriaAbdala <78234904+ValeriaAbdala@users.noreply.github.com>
2023-04-22 21:18:12 -0600

    Query for estudiantes with tutor

--- src/BussinessLogic/EstudianteDAO.java
+++ src/BussinessLogic/EstudianteDAO.java
@@ -18,12 +18,12 @@ import javafx.scene.control.CheckBox;
  * @author michikato
  */
 public class EstudianteDAO {
-
+    
     public boolean setEstudianteRegister(Estudiante estudiante, int clave) throws SQLException {
         boolean result = false;
         DataBaseConnection dataBaseConnection = new DataBaseConnection();
         Connection connection = dataBaseConnection.getConnection();
-
+        
         if (connection != null) {
             String query = ("INSERT INTO estudiantes (`matricula`, `nombre`, `apellidoPaterno`, `apellidoMaterno`, `clave`) VALUES (?, ?, ?, ?, ?);");
             PreparedStatement statement = connection.prepareStatement(query);
@@ -40,12 +40,12 @@ public class EstudianteDAO {
         connection.close();
         return result;
     }
-
+    
     public boolean validateExistEstudiante(String matricula) throws SQLException {
         boolean result = false;
         DataBaseConnection dataBaseConnection = new DataBaseConnection();
         Connection connection = dataBaseConnection.getConnection();
-
+        
         if (connection != null) {
             String query = ("SELECT COUNT(*) FROM estudiantes WHERE matricula = ?");
             PreparedStatement statement = connection.prepareStatement(query);
@@ -60,7 +60,7 @@ public class EstudianteDAO {
         connection.close();
         return result;
     }
-
+    
     public static ArrayList<Estudiante> obtenerEstudiantesPorTutorAcademico(int numeroPersonal) throws SQLException {
         ArrayList<Estudiante> estudiantes = new ArrayList<>();
         DataBaseConnection dataBaseConnection = new DataBaseConnection();
@@ -82,12 +82,12 @@ public class EstudianteDAO {
         }
         return estudiantes;
     }
-
+    
     public int getAllEstudiantesWithTutor(int clave) throws SQLException {
         int allEstudiantes = 0;
         DataBaseConnection dataBaseConnection = new DataBaseConnection();
         Connection connection = dataBaseConnection.getConnection();
-
+        
         if (connection != null) {
             String query = ("SELECT COUNT(*) AS TotalEstudiantes\n"
                     + "FROM estudiantes\n"
@@ -101,31 +101,35 @@ public class EstudianteDAO {
         connection.close();
         return allEstudiantes;
     }
-
+    
     public ArrayList<Estudiante> getEstudiantesByPrograma(String clave) throws SQLException {
-
+        
         ArrayList<Estudiante> estudiantes = new ArrayList<Estudiante>();
         DataBaseConnection dataBaseConnection = new DataBaseConnection();
         Connection connection = dataBaseConnection.getConnection();
-
+        
         if (connection != null) {
-            String query = ("SELECT * from Estudiantes WHERE clave = ?");
+            String query = ("select e.*, concat(u.nombre,' ',u.apellidoPaterno, ' ',u.apellidoMaterno) as nombreTutor  from estudiantes e \n"
+                    + "inner join programas_educativos pe on e.clave=pe.clave\n"
+                    + "left join usuarios u on u.numeroDePersonal =e.numeroDePersonal where pe.clave=?;");
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, clave);
             ResultSet resultSet = statement.executeQuery();
-
+            
             if (resultSet.next()) {
                 String nombre;
                 String apellidoMaterno;
                 String apellidoPaterno;
                 String matricula;
+                String nombreTutor;
                 CheckBox checkbox;
-
+                
                 do {
                     nombre = resultSet.getString("nombre");
                     apellidoMaterno = resultSet.getString("apellidoMaterno");
                     apellidoPaterno = resultSet.getString("apellidoPaterno");
                     matricula = resultSet.getString("matricula");
+                    nombreTutor = resultSet.getString("nombreTutor");
                     checkbox = new CheckBox();
                     Estudiante estudiante = new Estudiante();
                     estudiante.setNombre(nombre);
@@ -133,21 +137,22 @@ public class EstudianteDAO {
                     estudiante.setApellidoMaterno(apellidoMaterno);
                     estudiante.setMatricula(matricula);
                     estudiante.setCheckBoxEnSeleccion(checkbox);
+                    estudiante.setTutorName(nombreTutor);
                     estudiantes.add(estudiante);
-
+                    
                 } while (resultSet.next());
             }
         }
         connection.close();
-
+        
         return estudiantes;
     }
-
+    
     public int updateAsignacion(String matricula, int numeroDePersonal) throws SQLException {
         int result = 0;
         DataBaseConnection dataBaseConnection = new DataBaseConnection();
         Connection connection = dataBaseConnection.getConnection();
-
+        
         if (connection != null) {
             String query = ("UPDATE estudiantes set numeroDePersonal=? where matricula=?");
             PreparedStatement statement = connection.prepareStatement(query);
@@ -156,11 +161,10 @@ public class EstudianteDAO {
             int resultSet = statement.executeUpdate();
             result = statement.executeUpdate();
             
-
         }
         connection.close();
         return result;
-
+        
     }
-
+    
 }
