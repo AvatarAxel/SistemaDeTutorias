01c13a03e9ad83278a97fc38058a7596cebae9d7
Ulises Ramx <ulisesram19@gmail.com>
2023-05-03 18:14:27 -0600

    getReportesByTutor method updated

--- src/BussinessLogic/ReporteDeTutoriaAcademicaDAO.java
+++ src/BussinessLogic/ReporteDeTutoriaAcademicaDAO.java
@@ -73,7 +73,7 @@ public class ReporteDeTutoriaAcademicaDAO {
                     + "  u.apellidoPaterno,\n"
                     + "  u.apellidoMaterno,\n"
                     + "  (SELECT COUNT(*) FROM lista_de_asistencias WHERE esAsistente = 1 AND idReporteTutoria = r.idReporteTutoria) as totalAsistentes,\n"
-                    + "  (SELECT COUNT(*) FROM lista_de_asistencias WHERE idReporteTutoria = r.idReporteTutoria) as totalRegistrados\n"
+                    + "  (SELECT COUNT(*) FROM lista_de_asistencias WHERE enRiesgo = 1 AND idReporteTutoria = r.idReporteTutoria) as totalEnRiesgo,\n"
                     + "FROM \n"
                     + "  reportes_de_tutorias_academicas r\n"
                     + "  INNER JOIN usuarios u ON r.numeroDePersonal = u.numeroDePersonal\n"
@@ -87,7 +87,7 @@ public class ReporteDeTutoriaAcademicaDAO {
                 TutorAcademico tutorAcademico = new TutorAcademico();
                 reporteDeTutoriaAcademica.setComentariosGenerales(resultSet.getString("comentariosGenerales"));
                 reporteDeTutoriaAcademica.setIdReporteTutoria(resultSet.getInt("idReporteTutoria"));
-                reporteDeTutoriaAcademica.setNumeroDeAlumnosEnRiesgo(resultSet.getInt("totalRegistrados"));
+                reporteDeTutoriaAcademica.setNumeroDeAlumnosEnRiesgo(resultSet.getInt("totalEnRiesgo"));
                 reporteDeTutoriaAcademica.setNumeroDeTutoradosQueAsistieron(resultSet.getInt("totalAsistentes"));
                 tutorAcademico.setNombre(resultSet.getString("nombre"));
                 tutorAcademico.setApellidoPaterno(resultSet.getString("apellidoPaterno"));
